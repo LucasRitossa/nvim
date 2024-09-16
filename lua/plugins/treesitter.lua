ed92c8f363bbca65200d1023ff8e12a33675edbe
Lucas Ritossa <lucasr@posteo.us>
2024-09-16 09:04:24 -0400

    base

--- lua/plugins/treesitter.lua
+++
@@ -1,6 +0,0 @@
-require 'nvim-treesitter.configs'.setup {
-    sync_install = false,
-    highlight = { -- enable highlighting for all file types
-        enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-    },
-}
